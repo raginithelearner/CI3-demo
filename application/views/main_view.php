2b4c4d5ba8dda88bb77c2e250e8dfb9bf8764a58
raginithelearner <raginithelearner@gmail.com>
2022-11-13 22:29:51 +0530

    final_commit

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -70,54 +70,11 @@ $section_data = $this->main_model->get_pages($section_page)[0]['text'];
       </header>
       <!-- header section end -->
       
-      
       <section>
-<!-- banner section start -->
-<div class='banner_section '>
-   <div class="arrow" id="arrow-banner" >
-      <a class='carousel-control-prev' href='#costum_slider' role='button' data-slide='prev'>
-         <i class=''><img src='images/left-arrow.png'></i>
-      </a>
-      <a class='carousel-control-next' href='#costum_slider' role='button' data-slide='next'>
-         <i class=''><img src='images/right-arrow.png'></i>
-      </a>
-   </div>
-         <div class='container'>
-            <div id='costum_slider' class='carousel slide' data-ride='carousel'>
-               <div class='carousel-inner'>
-                  <div class='carousel-item active'>
-                     <h1 class='furniture_text'>FURNITURE</h1>
-                     <p class='there_text'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration</p>
-                     <div class='contact_bt_main'>
-                        <div class='contact_bt'><a href='<?=base_url('contact')?>'>Contact Us</a></div>
-                     </div>
-                  </div>
-                  <div class='carousel-item'>
-                     <h1 class='furniture_text'>FURNITURE</h1>
-                     <p class='there_text'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration</p>
-                     <div class='contact_bt_main'>
-                        <div class='contact_bt'><a href='../pages/contact.html'>Contact Us</a></div>
-                     </div>
-                  </div>
-                  <div class='carousel-item'>
-                     <h1 class='furniture_text'>FURNITURE</h1>
-                     <p class='there_text'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration</p>
-                     <div class='contact_bt_main'>
-                        <div class='contact_bt'><a href='../pages/contact.html'>Contact Us</a></div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <img src="images/furniture.webp" alt="">
-      </div>
-
-      </section>
-      <!-- <section>
          <?php
-            // echo $section_data;
+            echo $section_data;
          ?>
-      </section> -->
+      </section>
 
 
       <script>
@@ -201,9 +158,7 @@ $section_data = $this->main_model->get_pages($section_page)[0]['text'];
                   </p>
                </div>
             </div>
-            <div class="input-group mb-2 mt-1">
-               <span class="input-group-text" id="basic-addon2"><a href="#">Call Us:&emsp;<strong>+01&ensp;123 456 789</strong></a></span>
-            </div>
+
          </div>
       </footer>
       <!--  footer section end -->
@@ -212,13 +167,13 @@ $section_data = $this->main_model->get_pages($section_page)[0]['text'];
       <div class="container">
          <div class="social_icon">
             <ul>
-               <li><a href="#"><img src="images/fb-icon.png"></a></li>
-               <li><a href="#"><img src="images/twitter-icon.png"></a></li>
-               <li><a href="#"><img src="images/instagram-icon.png"></a></li>
-               <li><a href="#"><img src="images/linkedin-icon.png"></a></li>
+               <li><a ><img src="images/fb-icon.png"></a></li>
+               <li><a ><img src="images/twitter-icon.png"></a></li>
+               <li><a ><img src="images/instagram-icon.png"></a></li>
+               <li><a ><img src="images/linkedin-icon.png"></a></li>
             </ul>
          </div>
-         <p class="copyright_text">2020 All Rights Reserved. Design by <a href="https://html.design">Free html  Templates</a></p>
+         <p class="copyright_text">2022 All Rights Reserved. Design and Developed by <strong><a href="https://portfolio.contenik.com">Contenik.com</a></strong></p>
       </div>
       <!-- copyright section end -->
       <!-- Javascript files-->
